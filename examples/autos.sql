76be834f833c3e7c20ebd8615ef7b1fa4548e0ff
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2023-02-03 19:48:30 +0000

    Add autos example

---
+++ examples/autos.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS autos;
+
+CREATE TABLE autos( id integer, 
+                     model text, 
+                     merk text);
+INSERT INTO autos VALUES (1,'Kangoo','Renault');
+INSERT INTO autos VALUES (3,'Alpine','Renault');
+INSERT INTO autos VALUES (4,'Golf','Volkswagen');
+INSERT INTO autos VALUES (2,'Mégane','Renault');
+INSERT INTO autos VALUES (5,'Fiesta','Ford');
+INSERT INTO autos VALUES (6,'Polo','Volkswagen');
